fa9c87dafada6716df2c7c9383de00c0bd21f1fe
Mihir <Mihir@gmail.com>
2022-05-19 11:30:23 +0530

    added tf files

---
+++ Provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  subscription_id = "7fb434a9-5cea-4bc4-9e3f-190316e9b642"
+  client_id       = "fb920624-f74a-47a3-bf72-88e70e3eec45"
+  client_secret   = "fhk8Q~WxzcJrDEAgxutvJL4LiZ-pXImvOzv1LcXJ"
+  tenant_id       = "3169a04f-4ece-4bef-9ea5-09aba582ea3c"
+  features {}
+}
---
+++ vnet2.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "example" {
+  name     = "tf_rg5"
+  location = "West Europe"
+  tags = {
+  env = "dev"
+  createdby = "rama"
+}
+}
+resource "azurerm_virtual_network" "example" {
+  name                = "vnet2"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4","10.0.0.4"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+}
+  
+  tags = {
+    environment ="dev"
+}
+}
